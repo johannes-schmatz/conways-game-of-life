0b0fbe7179b6ee719afaedfa82a5556f05eee15b
Johannes Schmatz <johannes_schmatz@gmx.de>
2022-01-27 02:41:58 +0100

    add 1 dimensional consumer function, add javadoc

--- src/local/pixy/conwaysgame/test/TestUtil.java
+++ src/local/pixy/conwaysgame/test/TestUtil.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import local.pixy.conwaysgame.util.QuadConsumer;
@@ -14,31 +15,92 @@ import local.pixy.conwaysgame.util.TriConsumer;
  *
  */
 public class TestUtil {
+	private static final int xStart = 1;
+	private static final int xEnd = 100_000;
+	private static final Function<Integer, Integer> xOperation = (x) -> (int) (x * x * 0.5 + 3);
+	private static final int yStart = 1;
+	private static final int yEnd = 100_000;
+	private static final Function<Integer, Integer> yOperation = (x) -> (int) (x * x * 0.78 + 5 - x);
+	private static final int zStart = -20;
+	private static final int zEnd = 20;
+	private static final Function<Integer, Integer> zOperation = (x) -> (int) (x + 1);
+	private static final int wStart = -20;
+	private static final int wEnd = 20;
+	private static final Function<Integer, Integer> wOperation = (x) -> (int) (x + 1);
+
+	/**
+	 * Calls first a setter, then a getter, also verifies that the data that data is
+	 * {@linkplain Object#equals(Object)} to value given by the getter.
+	 * 
+	 * @param <T>       The type to give the set method and read from the getter.
+	 *                  The data input has also this type.
+	 * @param setMethod A reference to a setter.
+	 * @param getMethod A reference to a getter.
+	 * @param data      The data to set and read later with the getter.
+	 */
 	public static <T> void testSetGet(Consumer<T> setMethod, Supplier<T> getMethod, T data) {
 		setMethod.accept(data);
 		assertTrue(data.equals(getMethod.get()));
 	}
-	
-	public static void consumer2dimUser(BiConsumer<Integer, Integer> consumer) {
-		for(int i = 1; i < 100_000; i = (int) (i * i * 0.5 + 3)) {
-			for(int j = 1; j < 100_000; j = (int) (j * j * 0.78 + 5 - j)) {
-				consumer.accept(i, j);
-			}
+
+	/**
+	 * Calls a consumer multiple times with different values.
+	 * 
+	 * @param consumer A consumer to call
+	 * @see TestUtil#consumer2dimUser(BiConsumer)
+	 * @see TestUtil#consumer3dimUser(TriConsumer)
+	 * @see TestUtil#consumer4dimUser(QuadConsumer)
+	 */
+	public static void consumer1dimUser(Consumer<Integer> consumer) {
+		for (int x = TestUtil.xStart; x < TestUtil.xEnd; x = TestUtil.xOperation.apply(x)) {
+			consumer.accept(x);
 		}
 	}
-	
-	public static void consumer3dimUser(TriConsumer<Integer, Integer, Integer> consumer) {
-		TestUtil.consumer2dimUser((x, y) -> {
-			for(int k = -20; k < 20; k++) {
-				consumer.accept(x, y, k);
+
+	/**
+	 * Calls a consumer multiple times with different values.
+	 * 
+	 * @param consumer A consumer to call.
+	 * @see TestUtil#consumer1dimUser(Consumer)
+	 * @see TestUtil#consumer3dimUser(TriConsumer)
+	 * @see TestUtil#consumer4dimUser(QuadConsumer)
+	 */
+	public static void consumer2dimUser(BiConsumer<Integer, Integer> consumer) {
+		TestUtil.consumer1dimUser((x) -> {
+			for (int y = TestUtil.yStart; y < TestUtil.yEnd; y = TestUtil.yOperation.apply(y)) {
+				consumer.accept(x, y);
 			}
 		});
 	}
-	
+
+	/**
+	 * Calls a consumer multiple times with different values.
+	 * 
+	 * @param consumer A consumer to call
+	 * @see TestUtil#consumer1dimUser(Consumer)
+	 * @see TestUtil#consumer2dimUser(BiConsumer)
+	 * @see TestUtil#consumer4dimUser(QuadConsumer)
+	 */
+	public static void consumer3dimUser(TriConsumer<Integer, Integer, Integer> consumer) {
+		TestUtil.consumer2dimUser((x, y) -> {
+			for (int z = TestUtil.zStart; z < TestUtil.zEnd; z = TestUtil.zOperation.apply(z)) {
+				consumer.accept(x, y, z);
+			}
+		});
+	}
+
+	/**
+	 * Calls a consumer multiple times with different values.
+	 * 
+	 * @param consumer A consumer to call.
+	 * @see TestUtil#consumer1dimUser(Consumer)
+	 * @see TestUtil#consumer2dimUser(BiConsumer)
+	 * @see TestUtil#consumer3dimUser(TriConsumer)
+	 */
 	public static void consumer4dimUser(QuadConsumer<Integer, Integer, Integer, Integer> consumer) {
-		TestUtil.consumer3dimUser((x, y, a) -> {
-			for(int k = -20; k < 20; k++) {
-				consumer.accept(x, y, a, k);
+		TestUtil.consumer3dimUser((x, y, z) -> {
+			for (int w = TestUtil.wStart; w < TestUtil.wEnd; w = TestUtil.wOperation.apply(w)) {
+				consumer.accept(x, y, z, w);
 			}
 		});
 	}

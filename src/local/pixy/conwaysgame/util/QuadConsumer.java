035351f39cb41ae349207a0f749a387140905f03
Johannes Schmatz <johannes_schmatz@gmx.de>
2022-02-09 18:40:23 +0100

    add javadoc and remove andThen from TriConsumer and QuadConsumer

--- src/local/pixy/conwaysgame/util/QuadConsumer.java
+++ src/local/pixy/conwaysgame/util/QuadConsumer.java
@@ -1,20 +1,38 @@
 package local.pixy.conwaysgame.util;
 
-import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
- * @author pixy
+ * Represents an operation that accepts two input arguments and returns no
+ * result. This is the three-arity specialization of {@link Consumer}. Unlike
+ * most other functional interfaces, {@code QuadConsumer} is expected to operate
+ * via side-effects.
  *
+ * <p>
+ * This is a {@linkplain FunctionalInterface} whose functional method is
+ * {@link #accept(Object, Object, Object, Object)}.
+ *
+ * @param <S1> the type of the first argument to the operation
+ * @param <S2> the type of the second argument to the operation
+ * @param <S3> the type of the third argument to the operation
+ * @param <S4> the type of the fourth argument to the operation
+ *
+ * @see Consumer
+ * @see BiConsumer
+ * @see TriConsumer
+ * 
+ * @author pixy
  */
 @FunctionalInterface
 public interface QuadConsumer<S1, S2, S3, S4> {
+	/**
+	 * Performs this operation on the given arguments.
+	 *
+	 * @param a the first input argument
+	 * @param b the second input argument
+	 * @param c the third input argument
+	 * @param d the fourth input argument
+	 */
 	void accept(S1 a, S2 b, S3 c, S4 d);
-    default QuadConsumer<S1, S2, S3, S4> andThen(QuadConsumer<? super S1, ? super S2, ? super S3, ? super S4> after) {
-        Objects.requireNonNull(after);
-
-        return (a, b, c, d) -> {
-            accept(a, b, c, d);
-            after.accept(a, b, c, d);
-        };
-    }
 }
--- src/local/pixy/conwaysgame/util/TriConsumer.java
+++ src/local/pixy/conwaysgame/util/TriConsumer.java
@@ -1,20 +1,36 @@
 package local.pixy.conwaysgame.util;
 
-import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
- * @author pixy
+ * Represents an operation that accepts two input arguments and returns no
+ * result. This is the three-arity specialization of {@link Consumer}. Unlike
+ * most other functional interfaces, {@code TriConsumer} is expected to operate
+ * via side-effects.
  *
+ * <p>
+ * This is a {@linkplain FunctionalInterface} whose functional method is
+ * {@link #accept(Object, Object, Object)}.
+ *
+ * @param <S1> the type of the first argument to the operation
+ * @param <S2> the type of the second argument to the operation
+ * @param <S3> the type of the third argument to the operation
+ *
+ * @see Consumer
+ * @see BiConsumer
+ * @see QuadConsumer
+ * 
+ * @author pixy
  */
 @FunctionalInterface
 public interface TriConsumer<S1, S2, S3> {
+	/**
+	 * Performs this operation on the given arguments.
+	 *
+	 * @param a the first input argument
+	 * @param b the second input argument
+	 * @param c the third input argument
+	 */
 	void accept(S1 a, S2 b, S3 c);
-    default TriConsumer<S1, S2, S3> andThen(TriConsumer<? super S1, ? super S2, ? super S3> after) {
-        Objects.requireNonNull(after);
-
-        return (a, b, c) -> {
-            accept(a, b, c);
-            after.accept(a, b, c);
-        };
-    }
 }
